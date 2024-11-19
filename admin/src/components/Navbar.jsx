6b401dafb29a816ba8e422e61b07ed0412f9327a
khalil <5alil.landolsi@gmail.com>
2024-11-20 00:01:48 +0100

    khalil was here

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-
+import React from 'react'
 import {assets} from '../assets/admin_assets/assets'
-function Navbar() {
+
+function Navbar({setToken}) {
   return (
-    <div className='flex item-center py-2 px-[4%] justify-between'>
-      <img className='w-[max(10%, 80px)]' src= {assets.logo} alt=''/>
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'> Logout</button>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img className='w-[max(10%,80px)]' src= {assets.logo} alt=''/>
+      <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
