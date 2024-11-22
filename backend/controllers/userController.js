1405563297186924ff290edd0022664d2098f209
khalil <5alil.landolsi@gmail.com>
2024-11-22 22:35:54 +0100

    psssttttt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,13 +77,13 @@ res.json({success: true, token})
 const adminLogin = async (req, res) => {
     try {
 
-        const {email,password} = req. body
+        const {email,password} = req.body
 
         if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-            const token = jwt.sign(email+password,process.env. JWT_SECRET);
+            const token = jwt.sign(email+password,process.env.JWT_SECRET);
             res.json ({success:true, token})
         } else {
-            res. json ({success: false, message: "Invalid credentials"})
+            res.json ({success: false, message: "Invalid credentials"})
         }
 
     } catch (error) {
