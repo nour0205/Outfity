ef9744ee7c0a97fedd14f4238f1ca20459be298d
yousefsassy <youssef.sassi@medtech.tn>
2024-11-23 17:03:04 +0100

    youssef was here

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -13,6 +13,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const [token,setToken] = useState('');
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -88,6 +89,7 @@ const ShopContextProvider = (props) => {
         cartItems, addToCart, 
         getCartCount, updateQuantity,
         getCartAmount, navigate
+        
     }
 
     return (
