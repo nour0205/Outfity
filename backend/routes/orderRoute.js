7ae27e592cbad9201226d288b3cba56bc8085090
khalil <5alil.landolsi@gmail.com>
2024-12-04 18:05:42 +0100

    oni chan

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,1 +1,14 @@
-import express from 'express'
+import express from "express";
+import adminAuth from '../middleware/adminAuth.js'
+import {
+  placeOrder,
+  placeOrderStripe,
+  palceOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+} from "../controllers/orderController.js";
+
+
+const orderRouter = express.Router()
+orderRouter.post('/list',adminAuth, allOrders)
