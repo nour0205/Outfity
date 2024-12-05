111f28c5e800ac110369780af1ca8ea1118aa08d
khalil <5alil.landolsi@gmail.com>
2024-12-05 23:02:53 +0100

    khalil was here

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import adminAuth from '../middleware/adminAuth.js'
+import adminAuth from "../middleware/adminAuth.js";
 import {
   placeOrder,
   placeOrderStripe,
@@ -9,10 +9,18 @@ import {
   updateStatus,
 } from "../controllers/orderController.js";
 
-const orderRouter = express.Router()
+const orderRouter = express.Router();
 
 // Admin Features
-orderRouter.post('/list',adminAuth, allOrders)
-orderRouter.post('/status', adminAuth, updateStatus)
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
 
-//Payment
+// Payment Features
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.post("/razorpay", authUser, palceOrderRazorpay);
+
+// User Feature
+orderRouter.post("/userorders", authUser, userOrders);
+
+export default orderRouter;
