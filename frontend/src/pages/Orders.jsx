4da9843cb5d14d8c190e5a04b398b486fa582e26
SASSIX <youssef.sassi@medtech.tn>
2024-12-17 16:35:10 +0100

    youssef was here

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -15,11 +15,9 @@ const Orders = () => {
         return null;
       }
 
-      const response = await axios.get(
-        backendUrl + "/api/order/userorders",
-        {},
-        { headers: { token } }
-      );
+      const response = await axios.get(backendUrl + "/api/order/userorders", {
+        headers: { token },
+      });
       if (response.data.success) {
         let allOrdersItem = [];
         response.data.orders.map((order) => {
@@ -36,7 +34,7 @@ const Orders = () => {
     } catch (error) {}
   };
 
-  const deleteItem = async function (itemId, orderId) {
+  const deleteItem = async (itemId, orderId) => {
     try {
       const response = await axios.delete(`${backendUrl}/api/order/${itemId}`, {
         headers: { token },
@@ -71,61 +69,66 @@ const Orders = () => {
       </div>
 
       <div>
-        {orderData.map((item, index) => (
-          <div
-            key={index}
-            className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
-          >
-            <div className="flex items-start gap-6 text-sm">
-              <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
-              <div>
-                <p className="sm:text-base font-medium">{item.name}</p>
-                <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
-                  <p>
-                    {currency}
-                    {item.price}
+        {orderData.length > 0 ? (
+          orderData.map((item, index) => (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+            >
+              <div className="flex items-start gap-6 text-sm">
+                <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
+                <div>
+                  <p className="sm:text-base font-medium">{item.name}</p>
+                  <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
+                    <p>
+                      {currency}
+                      {item.price}
+                    </p>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Size: {item.size}</p>
+                  </div>
+                  <p className="mt-1">
+                    Date:
+                    <span className="ml-1 text-gray-400">
+                      {new Date(item.date).toDateString()}
+                    </span>
                   </p>
-                  <p>Quantity: {item.quantity}</p>
-                  <p>Size: {item.size}</p>
+                  <p className="mt-1">
+                    Payment:
+                    <span className="ml-1 text-gray-400">
+                      {item.paymentMethod}
+                    </span>
+                  </p>
                 </div>
-                <p className="mt-1">
-                  Date:
-                  <span className="ml-1 text-gray-400">
-                    {new Date(item.date).toDateString()}
-                  </span>
-                </p>
-                <p className="mt-1">
-                  Payment:
-                  <span className="ml-1 text-gray-400">
-                    {item.paymentMethod}
-                  </span>
-                </p>
+              </div>
+              <div className="md:w-1/2 flex justify-between">
+                <div className="flex items-center gap-2">
+                  <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                  <p className="text-sm md:text-base">{item.status}</p>
+                </div>
+                <button
+                  onClick={loadOrderData}
+                  className="border px-4 py-2 text-sm font-medium rounded-sm"
+                >
+                  Track Order
+                </button>
+                <button
+                  onClick={() => {
+                    deleteItem(item._id).then(() => {
+                      loadOrderData();
+                    });
+                  }}
+                  className="bg-red-500 text-white border px-4 py-2 text-sm font-medium rounded-sm"
+                >
+                  Remove Item
+                </button>
               </div>
             </div>
-            <div className="md:w-1/2 flex justify-between">
-              <div className="flex items-center gap-2">
-                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
-                <p className="text-sm md:text-base">{item.status}</p>
-              </div>
-              <button
-                onClick={loadOrderData}
-                className="border px-4 py-2 text-sm font-medium rounded-sm"
-              >
-                Track Order
-              </button>
-              <button
-                onClick={() => {
-                  deleteItem(item._id).then(() => {
-                    loadOrderData();
-                  });
-                }}
-                className="bg-red-500 text-white border px-4 py-2 text-sm font-medium rounded-sm"
-              >
-                Remove Item
-              </button>
-            </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          // Fallback when there are no orders
+          <p className="text-center text-gray-500 mt-8">No orders found.</p>
+        )}
       </div>
     </div>
   );
