80e85c9fc541e399985f47439fcf12db21187596
nour kouider <nourkouider05@gmail.com>
2024-12-17 16:51:21 +0100

    azerty

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,16 @@
-
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, requiered: true},
-    email: { type: String, requiered: true, unique: true},
-    password: { type: String, requiered: true},
-    cartData: { type: Object, default: {}}
- },{minimize:false})
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, requiered: true },
+    email: { type: String, requiered: true, unique: true },
+    password: { type: String, requiered: true },
+    cartData: { type: Object, default: {} },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { minimize: false }
+);
 
- const userModel = mongoose.models.user || mongoose.model('user',userSchema);
- 
- export default userModel
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
